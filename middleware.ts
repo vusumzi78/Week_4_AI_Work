ef486cc214f6f3e3608ed3bbada70ce04713799d
vusumzi78 <116901571+vusumzi78@users.noreply.github.com>
2024-08-22 12:19:42 +0200

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import {NextResponse} from 'next/server'
+
+export default clerkMiddleware();
+
+export function middleware(request){
+  const isAuthenticated = true;
+  if (isAuthenticated) {
+    return NextResponse.next();
+  }
+  return NextResponse.redirect(new URL('/login', request.url))
+}
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
